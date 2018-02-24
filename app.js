178c07dfb3c0b9cbb5e2cd86bedaf487b2cb3434
mcattx <mcattx@outlook.com>
2018-02-24 16:15:03 +0800

    fea: use static assets in koa

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const path = require('path');
 const Koa = require('koa');
 const Router = require('koa-router');
 const Views = require('koa-views');
+const serve = require('koa-static');
 
 const app = new Koa();
 const router = new Router();
 
+/** use static assets */
+app.use(serve(path.join(__dirname + '/assets')));
+
+/** use template engine in Koa */
 app.use(Views(path.join(__dirname + '/templates'), {
   map: {
     njk: 'nunjucks'
@@ -14,6 +19,7 @@ app.use(Views(path.join(__dirname + '/templates'), {
   extension: 'njk'
 }));
 
+/** define router */
 router.get('/', (ctx, next) => {
   return ctx.render('index', {
     username: 'macat',
@@ -23,6 +29,11 @@ router.get('/', (ctx, next) => {
     }
   })
 });
+router.get('/article', (ctx, next) => {
+  return ctx.render('article')
+});
+
+/** use Router in Koa */
 app
   .use(router.routes())
   .use(router.allowedMethods());
