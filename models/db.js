8ed75f549b55b8b025ade0c530cea2ee60080b7f
mcattx <mcattx@outlook.com>
2018-02-27 23:38:28 +0800

    feature: add mysql support

---
+++ models/db.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+})
+
+const User = sequelize.define('user', {
+  username: Sequelize.STRING,
+  birthday: Sequelize.DATE
+});
+
+sequelize.sync()
+  .then(() => User.create({
+    username: 'mcattx',
+    birthday: new Date(1991, 8 ,28)
+  }))
+  .then(jane => {
+    console.log(jane.toJSON());
+  })
